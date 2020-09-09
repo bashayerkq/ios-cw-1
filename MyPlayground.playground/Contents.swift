de3ddd372a93dcd4f67194ace5639fdbfaa118e5
bashayerkq <70640706+bashayerkq@users.noreply.github.com>
2020-09-09 20:54:58 +0300

    classwork done

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,22 @@
+
+let name = "bashayer"
+var height = 1.5
+var age = 15
+var hobby1 = "cooking"
+var hobby2 = "swimming"
+print("my name is \(name) my age is \(age) my height is \(height) my hobbies are \(hobby1) \(hobby2) "  )
+
+let friendName = "dana"
+var friendHeight = 1.6
+var friendAge = 18
+var friendHobby1 = "cooking"
+var friendHobby2 = "reading"
+
+var age_difference = age - friendAge
+print  (age_difference)
+hobby1 == friendHobby1 || hobby2 == friendHobby1 || hobby1 == friendHobby2 || hobby2 == friendHobby2
+age > friendAge
+height <= friendHeight
+age < friendAge
+
+
